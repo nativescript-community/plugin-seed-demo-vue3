1948f6c7eac635d0f0a15d5dae306ede6675d5c7
farfromrefug <dev@akylas.fr>
2025-10-07 17:18:04 +0200

    chore: tsc

--- app/components/Menu.vue
+++ app/components/Menu.vue
@@ -1,50 +1,49 @@
 <template>
-  <Frame>
-    <Page>
-      <ActionBar>
-        <Label text="Vue 3 Demo"/>
-      </ActionBar>
+    <Frame>
+        <Page>
+            <ActionBar>
+                <Label text="Vue 3 Demo" />
+            </ActionBar>
 
-      <ScrollView>
-        <StackLayout>
-          <StackLayout v-for="demo in demos" :key="demo.name">
-            <Button :text="demo.name" @tap="goToDemo(demo.component)"/>
-          </StackLayout>
-        </StackLayout>
-      </ScrollView>
-    </Page>
-  </Frame>
+            <ScrollView>
+                <StackLayout>
+                    <StackLayout v-for="demo in demos" :key="demo.name">
+                        <Button :text="demo.name" @tap="goToDemo(demo.component)" />
+                    </StackLayout>
+                </StackLayout>
+            </ScrollView>
+        </Page>
+    </Frame>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref, $navigateTo} from "nativescript-vue";
-import {demos} from '../../../demo-snippets/vue3/install';
+import { $navigateTo, onMounted, ref } from 'nativescript-vue';
+import { demos } from '../../../demo-snippets/vue3/install';
 
 declare const demoRedirect: string;
 
 onMounted(() => {
-  if (demoRedirect) {
-    const Demo = demos.find(({path}) => path === demoRedirect);
-    if (Demo) {
-      $navigateTo(Demo.component, {
-        animated: false
-      });
+    if (demoRedirect) {
+        const Demo = demos.find(({ path }) => path === demoRedirect);
+        if (Demo) {
+            $navigateTo(Demo.component, {
+                animated: false
+            });
+        }
     }
-  }
-})
+});
 const goToDemo = async (component: any) => {
-  try {
-    await $navigateTo(component, {
-      animated: true,
-      transition: {
-        name: 'slideLeft',
-        duration: 200,
-        curve: 'ease'
-      }
-    });
-  } catch (err) {
-    console.error(err, err.stack);
-  }
-}
-
+    try {
+        $navigateTo(component, {
+            animated: true,
+            transition: {
+                name: 'slideLeft',
+                duration: 200,
+                curve: 'ease'
+            }
+        });
+    } catch (err) {
+        console.error(err, err.stack);
+    }
+};
 </script>
