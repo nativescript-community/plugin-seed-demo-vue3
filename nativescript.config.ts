af026ff476c2a1fcc1e24cc61dd868dbbe1f243b
farfromrefuge <dev@akylas.fr>
2025-12-29 18:46:01 +0100

    chore: updated gradle

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -5,6 +5,7 @@ export default {
     appPath: 'app',
     appResourcesPath: 'App_Resources',
     android: {
+        gradleVersion: '8.14.3',
         v8Flags: '--expose_gc',
         markingMode: 'none'
     }
